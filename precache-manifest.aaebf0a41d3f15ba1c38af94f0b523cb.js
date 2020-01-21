ebb8040162044961856144ccf10112c22728482c
Damian <d.r.willems@gmail.com>
2020-01-21 21:37:23 +1300

    Updates

---
+++ precache-manifest.aaebf0a41d3f15ba1c38af94f0b523cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40010e12baaf05ba21f633f48eb8f7b",
+    "url": "/m-rolodex/index.html"
+  },
+  {
+    "revision": "d3f6468ea5ca4e87eb3b",
+    "url": "/m-rolodex/static/css/main.5c44e55c.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/m-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/m-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3f6468ea5ca4e87eb3b",
+    "url": "/m-rolodex/static/js/main.4e713dd6.chunk.js"
+  },
+  {
+    "revision": "2dbc399eb43b8a4e1018",
+    "url": "/m-rolodex/static/js/runtime-main.92dfb80f.js"
+  }
+]);
